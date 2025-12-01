8ad4728c70a3d72ff9c6e1c218e790f97e025580
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2025-12-01 22:12:58 +0530

    leftbar

--- client/src/components/leftBar/leftBar.jsx
+++ client/src/components/leftBar/leftBar.jsx
@@ -3,7 +3,26 @@ import "./leftBar.css";
 const LeftBar = () => {
     return (
         <div className="leftBar">
-            Left Bar
+            <div className="menuIcons">
+                <a href="/" className="menuIcon">
+                    <img src="/general/logo.png" alt="" className="logo" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/home.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/create.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/updates.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/messages.svg" alt="" />
+                </a>
+            </div>
+            <a href="/" className="menuIcon">
+                <img src="/general/messages.svg" alt="" />
+            </a>
         </div>
     )
 }
